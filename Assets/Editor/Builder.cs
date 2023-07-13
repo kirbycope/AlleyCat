d0175067a7bb60b45d7c1ec8b979a628ab703b29
Timothy Cope <kirbycope@gmail.com>
2023-07-12 21:14:30 -0700

    Update Builder.cs

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,11 +1,22 @@
-//https://docs.unity.com/embeddedlinux/en/manual/how-to-build-using-cli#Build_script
+// https://docs.unity.com/embeddedlinux/en/manual/how-to-build-using-cli#Build_script
+// https://fadhilnoer.medium.com/automating-unity-builds-part-1-ba0c60e8d06b
+// https://docs.unity3d.com/ScriptReference/BuildPipeline.BuildPlayer.html
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-// https://fadhilnoer.medium.com/automating-unity-builds-part-1-ba0c60e8d06b
 public class Builder
 {
+    [MenuItem("Build/Build Android")]
+    public static void BuildAndroid() {
+        BuildProject("../", "Android");
+    }
+
+    [MenuItem("Build/Build iOS")]
+    public static void BuildIos() {
+        BuildProject("../", "iOS");
+    }
+
     public static void BuildProject(string path, BuildTarget buildTarget)
     {
         var options = new BuildPlayerOptions
@@ -14,7 +25,15 @@ public class Builder
             target = buildTarget, 
             locationPathName = path,
         };
-
-        BuildPipeline.BuildPlayer(options);
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildSummary summary = report.summary;
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
     }
 }
