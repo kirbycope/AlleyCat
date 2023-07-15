849aacab71b683b25be24c2f94e247f9ac5c02ec
Timothy Cope <kirbycope@gmail.com>
2023-07-14 21:53:23 -0700

    navigation

---
+++ Assets/SampleScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class SampleScene : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        Button button = root.Q<Button>("Button");
+        button.clicked += () => SceneManager.LoadScene(sceneName: "Sandbox");
+    }
+}
