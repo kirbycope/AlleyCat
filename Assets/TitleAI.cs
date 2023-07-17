f2728f98e273fac98d88d4438b0389ff7a97dc20
Timothy Cope <kirbycope@gmail.com>
2023-07-16 18:22:04 -0700

    AI

--- Assets/TitleAI.cs
+++ Assets/TitleAI.cs
@@ -6,25 +6,55 @@ using UnityEngine.AI;
 public class TitleAI : MonoBehaviour
 {
 
-    UnityEngine.AI.NavMeshAgent agent;
+    private Animator animator;
+    private UnityEngine.AI.NavMeshAgent agent;
+    private float? sitTime;
+    private Vector3 target;
+    private int waypointIndex;
+
+    public float sitWaitTime;
     public Transform[] waypoints;
-    int waypointIndex;
-    Vector3 target;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = GetComponent<Animator>();
         UpdateDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If the target is less than 1 unit away, then increment the index/destination.
         if (Vector3.Distance(transform.position, target) < 1)
         {
-            IterateWaypointIndex();
-            UpdateDestination();
+            if (sitTime == null)
+            {
+                Debug.Log("sitting");
+                // Set sitTime
+                sitTime = Time.time;
+                // Switch animation to sitting
+                animator.SetBool("isMoving", false);
+                // Stop the navagation
+                agent.isStopped = true;
+            }
+            // If the sitWaitTime has elapsed, then...
+            if (Time.time - sitTime >= sitWaitTime)
+            {
+                Debug.Log("walking");
+                // End sitTime
+                sitTime = null;
+                // Resume walking animation
+                animator.SetBool("isMoving", true);
+                // Increment the Waypoint index
+                IterateWaypointIndex();
+                // Start the navigation
+                agent.isStopped = false;
+                // Update the new destination
+                UpdateDestination();
+            }
+            
         }
     }
 
@@ -32,7 +62,6 @@ public class TitleAI : MonoBehaviour
     {
         target = waypoints[waypointIndex].position;
         agent.SetDestination(target);
-        
     }
 
     void IterateWaypointIndex()
